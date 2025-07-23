a4da4860ed1406dfa68fa955e6836113d5cc5ec5
Mihail <mykh.shev4enko@gmail.com>
2025-07-23 19:44:22 +0300

    add hero section

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,34 +1,42 @@
-import { Link, NavLink } from "react-router";
+import { Link, NavLink } from "react-router-dom";
 import css from "./Navigation.module.css";
 
 export default function Navigation() {
   return (
-    <div className={`container ${css.header}`}>
-      <Link className={css.link} to="/">
-        <svg height={16} width={104}>
-          <use href="/logo/logo.svg" />
-        </svg>
-      </Link>
+    <header>
+      <div className={`container ${css.header}`}>
+        <Link className={css.link} to="/">
+          <svg height={16} width={104}>
+            <use href="/logo/logo.svg" />
+          </svg>
+        </Link>
 
-      <div className={css.wrap_link}>
-        <NavLink
-          className={({ isActive }) =>
-            isActive ? `${css.link} ${css.active}` : css.link
-          }
-          to="/"
-        >
-          Home
-        </NavLink>
+        <nav>
+          <ul className={css.ul}>
+            <li>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? `${css.link} ${css.active}` : css.link
+                }
+                to="/"
+              >
+                Home
+              </NavLink>
+            </li>
 
-        <NavLink
-          className={({ isActive }) =>
-            isActive ? `${css.link} ${css.active}` : css.link
-          }
-          to="/catalog"
-        >
-          Catalog
-        </NavLink>
+            <li>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? `${css.link} ${css.active}` : css.link
+                }
+                to="/catalog"
+              >
+                Catalog
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
       </div>
-    </div>
+    </header>
   );
 }
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,7 +1,22 @@
+import { useNavigate } from "react-router";
+import css from "./MainPage.module.css";
+
 export default function MainPage() {
+  const navigate = useNavigate();
+
+  const handleClick = () => navigate("/catalog");
+
   return (
-    <>
-      <h1>MainPage</h1>
-    </>
+    <section className={css.hero}>
+      <h1 className={css.title}>Find your perfect rental car</h1>
+
+      <p className={css.text}>
+        Reliable and budget-friendly rentals for any journey
+      </p>
+
+      <button className={css.button} onClick={handleClick} type="button">
+        View Catalog
+      </button>
+    </section>
   );
 }
